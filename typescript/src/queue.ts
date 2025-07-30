a36d8f46c5ec423543b24c5ce00157582c2935df
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-07-30 15:31:10 -0400

    refactor: remove queue info

--- typescript/src/queue.ts
+++ typescript/src/queue.ts
@@ -2,18 +2,12 @@ export interface Message {
   readonly body: string;
 }
 
-export interface QueueInfo {
-  readonly size: number;
-}
-
 export abstract class Queue {
   protected readonly RECV_BLOCK_SECS = 20;
 
   public abstract putMessage(body: string): Promise<void>;
 
   public abstract getMessage(): Promise<Message>;
-
-  public abstract getInfo(): Promise<QueueInfo>;
 
   public abstract close(): Promise<void>;
 }
