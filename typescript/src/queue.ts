7c6f7ea5c6c485ca3bee50690cc2e634b80c0f0c
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-07-30 15:56:37 -0400

    fix: add topic

--- typescript/src/queue.ts
+++ typescript/src/queue.ts
@@ -1,7 +1,7 @@
 export abstract class Queue {
-  public abstract put(message: string): Promise<void>;
+  public abstract put(topic: string, message: string): Promise<void>;
 
-  public abstract get(): Promise<string>;
+  public abstract get(topic: string): Promise<string>;
 
   public async close(): Promise<void> {}
 }
