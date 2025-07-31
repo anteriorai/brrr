d0be1f5e6c8dbc543dbd69ca698372491ff42c1b
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-07-31 14:31:19 -0400

    test: add in memory tests

--- typescript/src/backends/in-memory.test.ts
+++ typescript/src/backends/in-memory.test.ts
@@ -1,18 +1,15 @@
-import { suite, test } from "node:test";
-import { InMemoryCache, InMemoryQueue, InMemoryStore } from "./in-memory.ts";
+import { describe, test } from "node:test";
+import { InMemoryByteStore, InMemoryQueue } from "./in-memory.ts";
 import {
   cacheContractTest,
   queueContractTest,
   storeContractTest,
 } from "../store.test.ts";
 
-await suite(import.meta.filename, async () => {
-  await test(InMemoryStore.name, async () => {
-    await storeContractTest(() => new InMemoryStore());
-  });
-
-  await test(InMemoryCache.name, async () => {
-    await cacheContractTest(() => new InMemoryCache());
+await describe(import.meta.filename, async () => {
+  await test(InMemoryByteStore.name, async () => {
+    await storeContractTest(() => new InMemoryByteStore());
+    await cacheContractTest(() => new InMemoryByteStore());
   });
 
   await test(InMemoryQueue.name, async () => {
