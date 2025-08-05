14353767c38cd7e274e9399d473031b251ec15f4
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-05 14:04:31 -0400

    test: add unit tests

--- typescript/package.nix
+++ typescript/package.nix
@@ -1,11 +1,14 @@
-{ package-lock2nix }:
-
-let
-  brrr = package-lock2nix.mkNpmWorkspace {
-    name = "brrr-ts";
-    root = "./.";
-  };
-in
 {
-  inherit brrr;
+  package-lock2nix,
+  callPackage,
+  nodejs,
+}:
+
+{
+  brrr-ts =
+    (callPackage package-lock2nix.lib.package-lock2nix {
+      # Overriding the node.js version to support running TS natively.
+      inherit nodejs;
+    }).mkNpmModule
+      { src = ./.; };
 }
