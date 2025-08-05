4ef042af2a5623bdde438c04a3b6e57f4fe8c3f4
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-05 17:02:53 -0400

    test: codec contract test

--- typescript/src/codec.test.ts
+++ typescript/src/codec.test.ts
@@ -1,64 +1,17 @@
-import { suite, test } from "node:test";
-import { deepStrictEqual, notDeepStrictEqual } from "node:assert/strict";
+import { describe, test } from "node:test";
+import { ok } from "node:assert/strict";
 import type { Codec } from "./codec.ts";
 
 export async function codecContractTest(codec: Codec) {
-  await suite("store-contract", async () => {
-    const cases: Record<string, [unknown[], unknown[]]> = {
-      simple: [
-        [1, 2],
-        [1, 2],
-      ],
-      "simple-object": [[{ b: 2, a: 1 }], [{ a: 1, b: 2 }]],
-      "nested object": [
-        [{ b: { c: 3, a: 1 }, a: 2 }],
-        [{ a: 2, b: { a: 1, c: 3 } }],
-      ],
-      "array of objects": [
-        [
-          [
-            { b: 2, a: 1 },
-            { d: 4, c: 3 },
-          ],
-        ],
-        [
-          [
-            { a: 1, b: 2 },
-            { c: 3, d: 4 },
-          ],
-        ],
-      ],
-      "complex object": [
-        [{ a: 1, b: { c: 3, d: [4, 5] }, e: "6" }],
-        [{ e: "6", b: { d: [4, 5], c: 3 }, a: 1 }],
-      ],
-      nulls: [
-        [{ a: null, b: [1, 2, null], c: { d: null } }],
-        [{ c: { d: null }, b: [1, 2, null], a: null }],
-      ],
-      undefineds: [
-        [{ a: undefined, b: [1, 2, undefined], c: { d: undefined } }],
-        [{ c: { d: undefined }, b: [1, 2, undefined], a: undefined }],
-      ],
-      empties: [[{ a: {}, b: [] }], [{ b: [], a: {} }]],
-      "mixed types": [
-        [{ a: 1, b: "2", c: true, d: null }],
-        [{ d: null, c: true, b: "2", a: 1 }],
-      ],
-    } as const;
+  await describe("store-contract", async () => {
+    async function plus(a: number, b: string): Promise<number> {
+      return Number(a) + Number(b);
+    }
 
-    await suite("deterministic call hash", async () => {
-      for (const [name, args] of Object.entries(cases)) {
-        await test(name, async () => {
-          // sanity check
-          deepStrictEqual(...args);
-
-          deepStrictEqual(
-            await codec.encodeCall("foo", args[0]),
-            await codec.encodeCall("foo", args[1]),
-          );
-        });
-      }
+    await test("deterministic call hash", async () => {
+      const a = await codec.encodeCall("foo", [1, 2]);
+      const b = await codec.encodeCall("foo", [1, 2]);
+      ok(a.equals(b))
     });
 
     await suite(
@@ -83,9 +36,16 @@ export async function codecContractTest(codec: Codec) {
       const a = await codec.encodeCall("foo", [1, 2]);
       const b = await codec.encodeCall("foo", [2, 1]);
       const c = await codec.encodeCall("bar", [1, 2]);
-      notDeepStrictEqual(a, b);
-      notDeepStrictEqual(a, c);
-      notDeepStrictEqual(b, c);
+      ok(!a.equals(b));
+      ok(!a.equals(c));
+      ok(!b.equals(c));
     });
-  });
+
+    await test("round trip: encodeCall -> invokeTask -> decodeReturn", async () => {
+      const args = [1, "2"]
+      const call = await codec.encodeCall(plus.name, args);
+      const result = await codec.invokeTask(call, plus);
+      await codec.decodeReturn(plus.name, result);
+    });
+  })
 }
