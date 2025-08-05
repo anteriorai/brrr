bc356a1a0f262294acdb1eaf58eb23f6113d9dc6
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-05 14:35:10 -0400

    refactor: cleanup

--- typescript/src/backends/in-memory.test.ts
+++ typescript/src/backends/in-memory.test.ts
@@ -8,11 +8,11 @@ import {
 
 await suite(import.meta.filename, async () => {
   await test(InMemoryByteStore.name, async () => {
-    await storeContractTest(async () => new InMemoryByteStore());
-    await cacheContractTest(async () => new InMemoryByteStore());
+    await storeContractTest(() => new InMemoryByteStore());
+    await cacheContractTest(() => new InMemoryByteStore());
   });
 
   await test(InMemoryQueue.name, async () => {
-    await queueContractTest(async (topics) => new InMemoryQueue(topics));
+    await queueContractTest((topics) => new InMemoryQueue(topics));
   });
 });
