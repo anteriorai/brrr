d61b7b7ebd040829b2ee935c91890e4cfdd84125
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-05 17:02:53 -0400

    test: codec contract test

--- typescript/src/codec.ts
+++ typescript/src/codec.ts
@@ -3,9 +3,9 @@ import type { Call } from "./call.ts";
 export interface Codec {
   encodeCall(taskName: string, args: unknown[]): Promise<Call>;
 
-  invokeTask(
+  invokeTask<A extends unknown[], R>(
     call: Call,
-    task: (...args: unknown[]) => Promise<unknown>,
+    task: (...args: A) => Promise<R>,
   ): Promise<Uint8Array>;
 
   decodeReturn(taskName: string, payload: Uint8Array): Promise<unknown>;
