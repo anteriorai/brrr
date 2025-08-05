d7317495d074011c2abdd5e333734fc3a5f59925
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-05 17:08:59 -0400

    fix: add generics

--- typescript/src/codec.ts
+++ typescript/src/codec.ts
@@ -1,12 +1,12 @@
 import type { Call } from "./call.ts";
 
 export interface Codec {
-  encodeCall(taskName: string, args: unknown[]): Promise<Call>;
+  encodeCall<A extends unknown[]>(taskName: string, args: A): Call;
 
   invokeTask<A extends unknown[], R>(
     call: Call,
     task: (...args: A) => Promise<R>,
   ): Promise<Uint8Array>;
 
-  decodeReturn(taskName: string, payload: Uint8Array): Promise<unknown>;
+  decodeReturn(taskName: string, payload: Uint8Array): unknown;
 }
