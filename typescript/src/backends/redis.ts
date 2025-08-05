64e46a25413c94f2b4cfe652f0b21df8cfd2fe17
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-05 09:44:20 -0400

    chore: cleanup

--- typescript/src/backends/redis.ts
+++ typescript/src/backends/redis.ts
@@ -1,11 +1,4 @@
-import {
-  type RedisClientType,
-  type RedisFunctions,
-  type RedisModules,
-  type RedisScripts,
-  createClient,
-  type RedisClientOptions,
-} from "redis";
+import type { RedisClientType, RedisFunctions, RedisModules, RedisScripts } from "redis";
 import type { Queue } from "../queue.ts";
 import type { Cache } from "../store.ts";
 import {
@@ -18,11 +11,10 @@ import type { Encoding } from "node:crypto";
 
 type RedisQueuePayload = [number, number, string];
 
-export class Redis implements Queue, Cache, AsyncDisposable {
-  public static readonly encoding = "utf-8" satisfies Encoding;
-
-  public readonly timeout: number = 10 * 1000; // 10 seconds
-  public readonly client: RedisClientType<
+export class Redis implements Queue, Cache {
+  public static readonly encoding = 'utf-8' satisfies Encoding
+  private readonly timeout = 20;
+  private readonly client: RedisClientType<
     RedisModules,
     RedisFunctions,
     RedisScripts,
@@ -33,35 +25,25 @@ export class Redis implements Queue, Cache, AsyncDisposable {
     this.client = client;
   }
 
-  public async push(topic: string, message: string): Promise<void> {
+  public async put(topic: string, message: string): Promise<void> {
     if (!this.client.isOpen) {
       throw new QueueIsClosedError();
     }
-    const element = bencoder
-      .encode([
-        1,
-        Math.floor(Date.now() / 1000),
-        message,
-      ] satisfies RedisQueuePayload)
-      .toString();
+    const element = bencoder.encode([1, Math.floor(Date.now() / 1000), message] satisfies RedisQueuePayload).toString();
     await this.client.rPush(topic, element);
   }
 
-  public async pop(topic: string): Promise<string> {
+  public async get(topic: string): Promise<string> {
     const response = await this.client.blPop(topic, this.timeout);
     if (!response) {
-      throw new QueueIsEmptyError();
+      throw new QueueIsEmptyError()
     }
-    const data = Uint8Array.from(response.element);
-    const chunks = bencoder.decode(data, Redis.encoding) as RedisQueuePayload;
-    if (
-      chunks[0] !== 1 ||
-      Number.isInteger(chunks[1]) ||
-      typeof chunks[2] !== "string"
-    ) {
-      throw new InvalidMessageError(chunks);
+    const data = Uint8Array.from(response.element)
+    const chunks = bencoder.decode(data, Redis.encoding) as RedisQueuePayload
+    if (chunks[0] !== 1 || Number.isInteger(chunks[1]) || typeof chunks[2] !== "string") {
+      throw new InvalidMessageError(chunks)
     }
-    return chunks[2];
+    return chunks[2]
   }
 
   public async incr(key: string): Promise<number> {
@@ -70,9 +52,5 @@ export class Redis implements Queue, Cache, AsyncDisposable {
 
   public async close(): Promise<void> {
     await this.client.quit();
-  }
-
-  public async [Symbol.asyncDispose](): Promise<void> {
-    await this.close();
   }
 }
