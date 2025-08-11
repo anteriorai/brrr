13ee5bdf22ba3b05aad69735728a06eb29aeab16
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-11 18:14:40 -0400

    refactor: migration

---
+++ typescript/src/json-codec.ts
@@ -0,0 +1,50 @@
+import { type BinaryToTextEncoding, createHash } from "node:crypto";
+import { TextDecoder, TextEncoder } from "node:util";
+import type { Call } from "./call.ts";
+import type { Codec } from "./codec.ts";
+import { parse, stringify } from "superjson";
+
+export class JsonCodec implements Codec {
+  public static readonly algorithm = "sha256";
+  public static readonly binaryToTextEncoding =
+    "hex" satisfies BinaryToTextEncoding;
+
+  private static readonly encoder = new TextEncoder();
+  private static readonly decoder = new TextDecoder();
+
+  public async decodeReturn(_: string, payload: Uint8Array): Promise<unknown> {
+    const decoded = JsonCodec.decoder.decode(payload);
+    return parse(decoded);
+  }
+
+  public async encodeCall<A extends unknown[]>(
+    taskName: string,
+    args: A,
+  ): Promise<Call> {
+    const data = stringify(args);
+    const payload = JsonCodec.encoder.encode(data);
+    const callHash = await this.hashCall(taskName, args);
+    return { taskName, payload, callHash };
+  }
+
+  public async invokeTask<A extends unknown[], R>(
+    call: Call,
+    task: (...args: A) => Promise<R>,
+  ): Promise<Uint8Array> {
+    const decoded = JsonCodec.decoder.decode(call.payload);
+    const args = parse<A>(decoded);
+    const result = await task(...args);
+    const resultJson = stringify(result);
+    return JsonCodec.encoder.encode(resultJson);
+  }
+
+  private async hashCall<A extends unknown>(
+    taskName: string,
+    args: A,
+  ): Promise<string> {
+    const data = stringify([taskName, args]);
+    return createHash("sha256")
+      .update(data)
+      .digest(JsonCodec.binaryToTextEncoding);
+  }
+}
--- typescript/src/store.test.ts
+++ typescript/src/store.test.ts
@@ -1,5 +1,19 @@
-import { beforeEach, suite, test } from "node:test";
-import { deepStrictEqual, ok } from "node:assert/strict";
+import {
+  afterEach,
+  before,
+  beforeEach,
+  mock,
+  type MockTimersOptions,
+  suite,
+  test,
+} from "node:test";
+import {
+  deepStrictEqual,
+  doesNotReject,
+  ok,
+  rejects,
+  strictEqual,
+} from "node:assert/strict";
 import {
   type Cache,
   type MemKey,
@@ -8,14 +22,13 @@ import {
   type Store,
 } from "./store.ts";
 import type { Queue } from "./queue.ts";
-import { doesNotReject, rejects, strictEqual } from "node:assert";
 import {
   CompareMismatchError,
   NotFoundError,
   UnknownTopicError,
 } from "./errors.ts";
 import { InMemoryByteStore } from "./backends/in-memory.ts";
-import { Call } from "./call.ts";
+import type { Call } from "./call.ts";
 
 await suite(import.meta.filename, async () => {
   await suite(PendingReturns.name, async () => {
@@ -41,7 +54,17 @@ await suite(import.meta.filename, async () => {
     let memory: Memory;
 
     const fixture = {
-      call: new Call("test-task", new Uint8Array([1, 2, 3]), "test-call-hash"),
+      call: {
+        taskName: "test-task",
+        payload: new Uint8Array([1, 2, 3]),
+        callHash: "test-call-hash",
+      } satisfies Call,
+      pendingReturns: {
+        key: {
+          type: "pending_returns",
+          callHash: "test-pending-return-hash",
+        } satisfies MemKey,
+      },
     } as const;
 
     beforeEach(async () => {
@@ -53,18 +76,18 @@ await suite(import.meta.filename, async () => {
 
     await test("getCall", async () => {
       const retrieved = await memory.getCall(fixture.call.callHash);
-      ok(retrieved.equals(fixture.call));
+      deepStrictEqual(retrieved, fixture.call);
     });
 
     await test("setCall", async () => {
-      const newCall = new Call(
-        "new-task",
-        new Uint8Array([4, 5, 6]),
-        "new-call-hash",
-      );
+      const newCall: Call = {
+        taskName: "new-task",
+        payload: new Uint8Array([4, 5, 6]),
+        callHash: "new-call-hash",
+      };
       await memory.setCall(newCall);
       const retrieved = await memory.getCall(newCall.callHash);
-      ok(retrieved.equals(newCall));
+      deepStrictEqual(retrieved, newCall);
     });
 
     await test("hasValue", async () => {
@@ -196,9 +219,9 @@ export async function queueContractTest(factory: (topics: string[]) => Queue) {
       message: "test-message",
     } as const;
 
-    beforeEach(() => {
+    beforeEach(async () => {
       queue = factory([fixture.topic]);
-      queue.push(fixture.topic, fixture.message);
+      await queue.push(fixture.topic, fixture.message);
     });
 
     await test("Basic pop", async () => {
