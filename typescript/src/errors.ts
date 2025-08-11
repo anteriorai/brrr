c8db276829dff6aff7eb436cae01cac65f06eabb
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-11 18:00:28 -0400

    refactor: migration

--- typescript/src/errors.ts
+++ typescript/src/errors.ts
@@ -1,7 +1,77 @@
+import type { MemKey } from "./store.ts";
+
 abstract class BrrrError extends Error {
   protected constructor(message: string) {
     super();
     this.name = this.constructor.name;
     this.message = message;
+  }
+}
+
+export class QueueIsEmptyError extends BrrrError {
+  public constructor() {
+    super("Queue is empty");
+  }
+}
+
+export class QueueIsClosedError extends BrrrError {
+  public constructor() {
+    super("Queue is closed");
+  }
+}
+
+export class QueuePopTimeoutError extends BrrrError {
+  public constructor(timeout: number) {
+    super(`Queue pop timed out after ${timeout}ms`);
+  }
+}
+
+export class BencodeError extends BrrrError {
+  public constructor(data: unknown) {
+    super(`Bencode error: ${JSON.stringify(data)}`);
+  }
+}
+
+export class InvalidMessageError extends BrrrError {
+  public constructor(value: unknown) {
+    super(`Invalid message: ${JSON.stringify(value)}`);
+  }
+}
+
+export class UnknownTopicError extends BrrrError {
+  public constructor(topic: string) {
+    super(`Unknown topic: ${topic}`);
+  }
+}
+
+export class NotFoundError extends BrrrError {
+  public constructor(key: MemKey) {
+    super(`Not found: ${JSON.stringify(key)}`);
+  }
+}
+
+export class CompareMismatchError extends BrrrError {
+  public constructor(key: MemKey) {
+    super(`Compare mismatch for key ${JSON.stringify(key)}`);
+  }
+}
+
+export class CasRetryLimitReachedError extends BrrrError {
+  public constructor(retryLimit: number) {
+    super(`CAS retry limit reached (${retryLimit})`);
+  }
+}
+
+export class SpawnLimitError extends BrrrError {
+  public constructor(limit: number, rootId: string, callHash: string) {
+    super(
+      `Spawn limit of ${limit} reached for rootId ${rootId} and callHash ${callHash}`,
+    );
+  }
+}
+
+export class TaskNotFoundError extends BrrrError {
+  public constructor(taskName: string) {
+    super(`Task not found: ${taskName}`);
   }
 }
--- typescript/src/queue.ts
+++ typescript/src/queue.ts
@@ -1,19 +1,7 @@
-export interface Message {
-  readonly body: string;
-}
+export interface Queue {
+  readonly timeout: number;
 
-export interface QueueInfo {
-  readonly size: number;
-}
+  push(topic: string, message: string): Promise<void>;
 
-export abstract class Queue {
-  protected readonly RECV_BLOCK_SECS = 20;
-
-  public abstract putMessage(body: string): Promise<void>;
-
-  public abstract getMessage(): Promise<Message>;
-
-  public abstract getInfo(): Promise<QueueInfo>;
-
-  public abstract close(): Promise<void>;
+  pop(topic: string): Promise<string>;
 }
