bd7f50e78b71337c65d1dd38f01b6118dfae748d
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-11 18:14:40 -0400

    refactor: migration

--- typescript/src/naive-codec.test.ts
+++
@@ -1,9 +0,0 @@
-import { suite, test } from "node:test";
-import { NaiveCodec } from "./naive-codec.ts";
-import { codecContractTest } from "./codec.test.ts";
-
-await suite(import.meta.filename, async () => {
-  await test(NaiveCodec.name, async () => {
-    await codecContractTest(new NaiveCodec());
-  });
-});
