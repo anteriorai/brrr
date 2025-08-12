aa28ec7922691216ef066cfcd8594e248aa42e72
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-12 09:23:19 -0400

    chore: deps

---
+++ typescript/src/backends/redis.ts
@@ -0,0 +1,78 @@
+import {
+  type RedisClientType,
+  type RedisFunctions,
+  type RedisModules,
+  type RedisScripts,
+  createClient,
+  type RedisClientOptions,
+} from "redis";
+import type { Queue } from "../queue.ts";
+import type { Cache } from "../store.ts";
+import {
+  InvalidMessageError,
+  QueueIsClosedError,
+  QueueIsEmptyError,
+} from "../errors.ts";
+import { bencoder } from "../bencode.ts";
+import type { Encoding } from "node:crypto";
+
+type RedisQueuePayload = [number, number, string];
+
+export class Redis implements Queue, Cache, AsyncDisposable {
+  public static readonly encoding = "utf-8" satisfies Encoding;
+
+  public readonly timeout: number = 10 * 1000; // 10 seconds
+  public readonly client: RedisClientType<
+    RedisModules,
+    RedisFunctions,
+    RedisScripts,
+    3
+  >;
+
+  public constructor(client: typeof this.client) {
+    this.client = client;
+  }
+
+  public async push(topic: string, message: string): Promise<void> {
+    if (!this.client.isOpen) {
+      throw new QueueIsClosedError();
+    }
+    const element = bencoder
+      .encode([
+        1,
+        Math.floor(Date.now() / 1000),
+        message,
+      ] satisfies RedisQueuePayload)
+      .toString();
+    await this.client.rPush(topic, element);
+  }
+
+  public async pop(topic: string): Promise<string> {
+    const response = await this.client.blPop(topic, this.timeout);
+    if (!response) {
+      throw new QueueIsEmptyError();
+    }
+    const data = Uint8Array.from(response.element);
+    const chunks = bencoder.decode(data, Redis.encoding) as RedisQueuePayload;
+    if (
+      chunks[0] !== 1 ||
+      Number.isInteger(chunks[1]) ||
+      typeof chunks[2] !== "string"
+    ) {
+      throw new InvalidMessageError(chunks);
+    }
+    return chunks[2];
+  }
+
+  public async incr(key: string): Promise<number> {
+    return this.client.incr(key);
+  }
+
+  public async close(): Promise<void> {
+    await this.client.quit();
+  }
+
+  public async [Symbol.asyncDispose](): Promise<void> {
+    await this.close();
+  }
+}
