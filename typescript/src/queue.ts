2dc0f872789b970f75dd113bd3f4f5ab4044a0db
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-13 15:30:23 -0400

    fix: nothrow

--- typescript/src/queue.ts
+++ typescript/src/queue.ts
@@ -2,8 +2,20 @@ export interface Message {
   readonly body: string;
 }
 
+export type QueuePopResult =
+  | {
+      kind: "QueueIsClosed";
+    }
+  | {
+      kind: "QueueIsEmpty";
+    }
+  | {
+      kind: "Ok";
+      message: Message;
+    };
+
 export interface Queue {
   push(topic: string, message: Message): Promise<void>;
 
-  pop(topic: string): Promise<Message>;
+  pop(topic: string): Promise<QueuePopResult>;
 }
