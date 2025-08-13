bfab824e1be49730e233a58d67031906b167cd8f
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-13 10:51:11 -0400

    chore: review

--- typescript/src/queue.ts
+++ typescript/src/queue.ts
@@ -1,5 +1,9 @@
+export interface Message {
+  body: string;
+}
+
 export interface Queue {
-  push(topic: string, message: string): Promise<void>;
+  push(topic: string, message: Message): Promise<void>;
 
   pop(topic: string): Promise<string>;
 }
