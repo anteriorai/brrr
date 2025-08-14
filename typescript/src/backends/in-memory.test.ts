6b06c3aeaa285c3d5eb00fe20aa7d3d55f90e41f
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-14 16:30:28 -0400

    move

--- typescript/src/backends/in-memory.test.ts
+++ typescript/src/backends/in-memory.test.ts
@@ -14,16 +14,5 @@ await suite(import.meta.filename, async () => {
 
   await test(InMemoryQueue.name, async () => {
     await queueContractTest((topics) => new InMemoryQueue(topics));
-
-    await test("join works over multiple topics", async () => {
-      const topics = ["topic-1", "topic-2"];
-      const queue = new InMemoryQueue(topics);
-      await queue.join();
-      await queue.push("topic-1", { body: "task" });
-      await queue.push("topic-2", { body: "task" });
-      const join = queue.join();
-      await Promise.all([queue.pop("topic-1"), queue.pop("topic-2")]);
-      await join;
-    });
   });
 });
