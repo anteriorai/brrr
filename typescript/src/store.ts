04ecff638b491555d7c45c75d3b871d702e85499
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-15 11:31:17 -0400

    wip

--- typescript/src/store.ts
+++ typescript/src/store.ts
@@ -2,7 +2,7 @@ import type { Call } from "./call.ts";
 import { bencoder } from "./bencode.ts";
 import { TextDecoder } from "node:util";
 import type { Encoding } from "node:crypto";
-import { NotFoundError } from "./errors.ts";
+import { CasRetryLimitReachedError, NotFoundError } from "./errors.ts";
 
 export interface PendingReturnsPayload {
   readonly scheduled_at: number;
@@ -84,7 +84,9 @@ export interface Cache {
 
 export class Memory {
   private static readonly casRetryLimit = 100;
-  private static decoder = new TextDecoder("ascii");
+  private static readonly encoding = "ascii" satisfies Encoding;
+  private static readonly decoder = new TextDecoder(Memory.encoding);
+
   private readonly store: Store;
 
   public constructor(store: Store) {
@@ -133,10 +135,11 @@ export class Memory {
   }
 
   public async getValue(callHash: string): Promise<Uint8Array | undefined> {
-    return this.store.get({
+    const value = this.store.get({
       type: "value",
       callHash,
     });
+    if (!value) {}
   }
 
   public async setValue(callHash: string, payload: Uint8Array): Promise<void> {
@@ -147,5 +150,96 @@ export class Memory {
       },
       payload,
     );
+  }
+
+  public async addPendingReturns(
+    callHash: string,
+    newReturn: string,
+    scheduleJob: () => Promise<void>,
+  ): Promise<boolean> {
+    const memKey: MemKey = {
+      type: "pending_returns",
+      callHash,
+    };
+    let shouldStoreAgain = false;
+    let existingEncoded: Uint8Array | undefined;
+    let existing: PendingReturns;
+    return this.withCas(async () => {
+      try {
+        existingEncoded = await this.store.get(memKey);
+        existing = PendingReturns.decode(existingEncoded);
+        if (!existing.returns.has(newReturn)) {
+          existing = new PendingReturns(
+            existing.scheduledAt,
+            new Set([...existing.returns, newReturn]),
+          );
+          shouldStoreAgain = true;
+        }
+      } catch (err) {
+        if (!(err instanceof NotFoundError)) {
+          throw err;
+        }
+        existing = new PendingReturns(undefined, new Set([newReturn]));
+        existingEncoded = existing.encode();
+        await this.store.setNewValue(memKey, existingEncoded);
+      }
+      const alreadyPending = !!existing.scheduledAt;
+      let shouldSchedule = false;
+      if (!alreadyPending) {
+        existing = new PendingReturns(
+          Math.floor(Date.now() / 1000),
+          existing.returns,
+        );
+        shouldStoreAgain = true;
+        shouldSchedule = true;
+      }
+      if (shouldStoreAgain) {
+        await this.store.compareAndSet(
+          memKey,
+          existing.encode(),
+          existingEncoded,
+        );
+        if (shouldSchedule) {
+          await scheduleJob();
+        }
+      }
+      return alreadyPending;
+    });
+  }
+
+  public async withPendingReturnsRemove(
+    callHash: string,
+    f: (returns: ReadonlySet<string>) => Promise<void>,
+  ) {
+    const memKey: MemKey = {
+      type: "pending_returns",
+      callHash,
+    };
+    const handled = new Set<string>();
+    return this.withCas(async () => {
+      let pendingEncoded: Uint8Array;
+      try {
+        pendingEncoded = await this.store.get(memKey);
+      } catch (err) {
+        if (err instanceof NotFoundError) {
+          return f(new Set());
+        }
+        throw err;
+      }
+      const toHandle =
+        PendingReturns.decode(pendingEncoded).returns.difference(handled);
+      await f(toHandle);
+      toHandle.forEach((it) => handled.add(it));
+      return this.store.compareAndDelete(memKey, pendingEncoded);
+    });
+  }
+
+  private async withCas(f: () => Promise<boolean>): Promise<void> {
+    for (let i = 0; i < Memory.casRetryLimit; i++) {
+      if (await f()) {
+        return;
+      }
+    }
+    throw new CasRetryLimitReachedError(Memory.casRetryLimit);
   }
 }
