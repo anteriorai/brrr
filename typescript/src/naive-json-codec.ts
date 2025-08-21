b25eaa02ae7fa9a2229ced7633b4a143da4e6ddf
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-21 11:41:49 -0400

    add docs

--- typescript/src/naive-json-codec.ts
+++ typescript/src/naive-json-codec.ts
@@ -7,6 +7,7 @@ import { decoder, encoder } from "./internal-codecs.ts";
 /**
  * Naive JSON codec that uses `superjson` for serialization and deserialization.
  *
+ * @remarks
  * It tries its best to ensure that the serialized data is deterministic by
  * sorting object keys recursively before serialization, but it's not
  * reccommended for production use; the primary purpose of this codec is
@@ -49,7 +50,7 @@ export class NaiveJsonCodec implements Codec {
     args: A,
   ): Promise<string> {
     const data = stringify([taskName, args]);
-    return createHash(NaiveJsonCodec.algorithm)
+    return createHash("sha256")
       .update(data)
       .digest(NaiveJsonCodec.binaryToTextEncoding);
   }
