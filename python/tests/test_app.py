31bc3cc248ee2383dd378e6beb12de2c2ed5da6d
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-21 16:14:51 -0400

    merge

--- python/tests/test_app.py
+++ python/tests/test_app.py
@@ -1,16 +1,16 @@
 import asyncio
+from collections import Counter
 import dataclasses
 import typing
-from collections import Counter
 from typing import cast
 
+from brrr.local_app import local_app, LocalBrrr
 import pytest
 
 import brrr
 from brrr import ActiveWorker, AppWorker, AppConsumer
-from brrr import Connection, Defer, DeferredCall, NotFoundError, Request, Response
 from brrr.backends.in_memory import InMemoryByteStore, InMemoryQueue
-from brrr.local_app import local_app, LocalBrrr
+from brrr import Connection, Defer, DeferredCall, NotFoundError, Request, Response
 from brrr.pickle_codec import PickleCodec
 
 from .parametrize import names
@@ -478,7 +478,7 @@ async def test_app_loop_resumable(topic: str) -> None:
         pass
 
     @brrr.handler_no_arg
-    async def bar(a: int) -> int:
+    async def foo(a: int) -> int:
         nonlocal errors
         if errors:
             errors -= 1
@@ -486,14 +486,8 @@ async def test_app_loop_resumable(topic: str) -> None:
         await queue.close()
         return a
 
-    @brrr.handler
-    async def foo(app: ActiveWorker, a: int) -> int:
-        return await app.call(bar)(a)
-
     async with brrr.serve(queue, store, store) as conn:
-        app = AppWorker(
-            handlers=dict(foo=foo, bar=bar), codec=PickleCodec(), connection=conn
-        )
+        app = AppWorker(handlers=dict(foo=foo), codec=PickleCodec(), connection=conn)
         while True:
             try:
                 await app.schedule(foo, topic=topic)(3)
