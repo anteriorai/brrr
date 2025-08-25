d84f0654f7f97e6bf38984c029ed29843d237924
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-25 17:34:55 -0400

    add flush test

--- typescript/src/backends/in-memory.test.ts
+++ typescript/src/backends/in-memory.test.ts
@@ -1,10 +1,7 @@
-import { suite, test } from "node:test";
+import { beforeEach, suite, test } from "node:test";
 import { InMemoryCache, InMemoryQueue, InMemoryStore } from "./in-memory.ts";
-import {
-  cacheContractTest,
-  queueContractTest,
-  storeContractTest,
-} from "../store.test.ts";
+import { cacheContractTest, queueContractTest, storeContractTest, } from "../store.test.ts";
+import { deepStrictEqual, strictEqual } from "node:assert/strict";
 
 await suite(import.meta.filename, async () => {
   await test(InMemoryStore.name, async () => {
@@ -17,5 +14,22 @@ await suite(import.meta.filename, async () => {
 
   await test(InMemoryQueue.name, async () => {
     await queueContractTest((topics) => new InMemoryQueue(topics));
+
+    const topic = "test-topic";
+    let queue: InMemoryQueue;
+
+    beforeEach(() => {
+      queue = new InMemoryQueue([topic]);
+    });
+
+    await test("flushing automatically closes when empty", async () => {
+      queue.flush();
+      await queue.push(topic, { body: "a" });
+      await queue.push(topic, { body: "b" });
+      deepStrictEqual(await queue.pop(topic), { kind: "Ok", value: { body: "a" } })
+      deepStrictEqual(await queue.pop(topic), { kind: "Ok", value: { body: "b" } })
+      const result = await queue.pop(topic);
+      strictEqual(result.kind, "QueueIsClosed");
+    });
   });
 });
