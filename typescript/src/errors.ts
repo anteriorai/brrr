28a22768d938a8c3e08e97d6feb13464087e1dfe
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-25 13:57:52 -0400

    review: robin

--- typescript/src/errors.ts
+++ typescript/src/errors.ts
@@ -39,9 +39,3 @@ export class TaskNotFoundError extends BrrrError {
     super(`Task not found: ${taskName}`);
   }
 }
-
-export class QueueIsClosedError extends BrrrError {
-  public constructor() {
-    super("Queue is closed");
-  }
-}
