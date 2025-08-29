ba838cb9cf31e7826b3b54b59f662c49b3c6c77a
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-08-29 10:48:01 -0400

    cleanup

--- typescript/src/backends/redis.test-integration.ts
+++ typescript/src/backends/redis.test-integration.ts
@@ -1,39 +1,53 @@
-import { suite } from "node:test";
+import { afterEach, beforeEach, suite, test } from "node:test";
 import { cacheContractTest } from "../store.test.ts";
-import { ok } from "node:assert/strict";
+import { ok, strictEqual } from "node:assert/strict";
 import { env } from "node:process";
-import {
-  createClient,
-  type RedisFunctions,
-  type RedisModules,
-  type RedisScripts,
-} from "redis";
+import { createClient } from "redis";
 import { Redis } from "./redis.ts";
 
 await suite(import.meta.filename, async () => {
   ok(env.BRRR_TEST_REDIS_URL);
 
-  const client = createClient<
-    RedisModules,
-    RedisFunctions,
-    RedisScripts,
-    3,
-    {}
-  >({
+  const client = createClient({
+    RESP: 3,
     url: env.BRRR_TEST_REDIS_URL,
   });
 
   let redis: Redis;
 
-  await cacheContractTest(
-    async () => {
-      redis = new Redis(client);
-      await redis.connect();
-      await client.flushAll();
-      return redis;
-    },
-    async () => {
-      await redis.close();
-    },
-  );
+  async function createRedis() {
+    redis = new Redis(client);
+    await redis.connect();
+    return redis;
+  }
+
+  async function closeRedis() {
+    await client.flushAll();
+    await redis.close();
+  }
+
+  await cacheContractTest(createRedis, closeRedis);
+
+  await suite("as message queue", async () => {
+    const topic = "test-topic";
+    const message = "some-message";
+
+    beforeEach(createRedis);
+    afterEach(closeRedis);
+
+    await test("push & pop message", async () => {
+      await redis.push(topic, message);
+      strictEqual(await redis.pop(topic), message);
+    });
+
+    await test("FIFO", async () => {
+      const messages = ["first", "second", "third"];
+      for (const message of messages) {
+        await redis.push(topic, message);
+      }
+      for (const message of messages) {
+        strictEqual(await redis.pop(topic), message);
+      }
+    });
+  });
 });
