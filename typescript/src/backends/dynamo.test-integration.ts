4ab042119e4e85b30be4081de97fd028535ebd2b
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-09-03 16:40:24 -0400

    fix envvar

--- typescript/src/backends/dynamo.test-integration.ts
+++ typescript/src/backends/dynamo.test-integration.ts
@@ -1,19 +1,11 @@
-import { suite, test } from "node:test";
+import { suite } from "node:test";
 import { Dynamo } from "./dynamo.ts";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { storeContractTest } from "../store.test.ts";
 import { randomUUID } from "node:crypto";
-import { ok, strictEqual } from "node:assert/strict";
-import { env } from "node:process";
 
 await suite(import.meta.filename, async () => {
-  ok(env.AWS_DEFAULT_REGION);
-  ok(env.AWS_ENDPOINT_URL);
-
-  const client = new DynamoDBClient({
-    region: env.AWS_DEFAULT_REGION,
-    endpoint: env.AWS_ENDPOINT_URL,
-  });
+  const client = new DynamoDBClient();
 
   let dynamo: Dynamo;
 
