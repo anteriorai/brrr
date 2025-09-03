f3af20ad72eff98f7e1fcdb1561a2a1e640aa3c8
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-09-03 16:40:24 -0400

    fix envvar

--- nix/brrr-integration-common.nix
+++ nix/brrr-integration-common.nix
@@ -21,6 +21,7 @@
 
   runtimeEnv = {
     AWS_DEFAULT_REGION = "us-east-1";
+    AWS_REGION = "us-east-1";
     AWS_ENDPOINT_URL = "http://datastores:8000";
     AWS_ACCESS_KEY_ID = "fake";
     AWS_SECRET_ACCESS_KEY = "fake";
