c15204cb21617d43b0d98d83424623b0fec78230
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-09-16 00:12:12 -0400

    fmt

--- typescript/src/tagged-tuple.test.ts
+++ typescript/src/tagged-tuple.test.ts
@@ -21,34 +21,34 @@ await suite(import.meta.filename, async () => {
     await suite("fromTuple", async () => {
       await test("type test", async () => {
         // correct types
-        const _: Foo = Foo.fromTuple(0, 42, "hello")
+        const _: Foo = Foo.fromTuple(0, 42, "hello");
         // @ts-expect-error wrong tag
-        throws(() => Foo.fromTuple(1, 42, "hello"))
+        throws(() => Foo.fromTuple(1, 42, "hello"));
         // @ts-expect-error wrong args
-        Foo.fromTuple(0, "wrong", "args")
-      })
+        Foo.fromTuple(0, "wrong", "args");
+      });
       await test("create instance", async () => {
         const foo = Foo.fromTuple(0, 42, "hello");
-        ok(foo instanceof Foo)
-        ok(foo.bar === 42)
-        ok(foo.baz === "hello")
-      })
-    })
+        ok(foo instanceof Foo);
+        ok(foo.bar === 42);
+        ok(foo.baz === "hello");
+      });
+    });
 
     await suite("asTuple", async () => {
       await test("basic", async () => {
-        const foo = Foo.fromTuple(0, 42, "hello")
-        deepStrictEqual(foo.asTuple(), [0, 42, "hello"])
-      })
-    })
-  })
+        const foo = Foo.fromTuple(0, 42, "hello");
+        deepStrictEqual(foo.asTuple(), [0, 42, "hello"]);
+      });
+    });
+  });
 
   await suite(TaggedTupleStrings.name, async () => {
     class Foo extends TaggedTupleStrings {
       public static readonly tag = 0;
 
-      public readonly bar: number
-      public readonly baz: string
+      public readonly bar: number;
+      public readonly baz: string;
 
       constructor(bar: number, baz: string) {
         super();
@@ -58,10 +58,10 @@ await suite(import.meta.filename, async () => {
     }
 
     await suite("encode and decode", async () => {
-      const foo: Foo = Foo.fromTuple(0, 42, "hello")
-      const encoded = foo.encode()
-      const decoded: Foo = Foo.decode(encoded)
-      deepStrictEqual(decoded, foo)
-    })
-  })
-})
+      const foo: Foo = Foo.fromTuple(0, 42, "hello");
+      const encoded = foo.encode();
+      const decoded: Foo = Foo.decode(encoded);
+      deepStrictEqual(decoded, foo);
+    });
+  });
+});
--- typescript/src/tagged-tuple.ts
+++ typescript/src/tagged-tuple.ts
@@ -7,7 +7,11 @@ type Tagged<T, A extends unknown[], Tag extends number> = {
 };
 
 export abstract class TaggedTuple {
-  public static fromTuple<T extends TaggedTuple, A extends unknown[], Tag extends number>(this: Tagged<T, A, Tag>, tag: NoInfer<Tag>, ...tuple: A): T {
+  public static fromTuple<
+    T extends TaggedTuple,
+    A extends unknown[],
+    Tag extends number,
+  >(this: Tagged<T, A, Tag>, tag: NoInfer<Tag>, ...tuple: A): T {
     if (tag !== this.tag) {
       throw new Error(`Tag mismatch: expected ${this.tag}, got ${tag}`);
     }
@@ -21,16 +25,24 @@ export abstract class TaggedTuple {
 }
 
 export abstract class TaggedTupleStrings extends TaggedTuple {
-  private static readonly encoding: Encoding = 'utf-8'
+  private static readonly encoding: Encoding = "utf-8";
 
   public encode(): Uint8Array {
-    const tuple = this.asTuple()
-    return bencoder.encode(tuple)
+    const tuple = this.asTuple();
+    return bencoder.encode(tuple);
   }
 
-  public static decode<T extends TaggedTuple, A extends unknown[], Tag extends number>(this: Tagged<T, A, Tag>, data: Uint8Array): T {
-    const decoded = bencoder.decode(data, TaggedTupleStrings.encoding) as [Tag, ...A];
-    return (this as any).fromTuple(...decoded)
+  public static decode<
+    T extends TaggedTuple,
+    A extends unknown[],
+    Tag extends number,
+  >(this: Tagged<T, A, Tag>, data: Uint8Array): T {
+    const decoded = bencoder.decode(data, TaggedTupleStrings.encoding) as [
+      Tag,
+      ...A,
+    ];
+    // cheating here - but we have enough type and test coverage to be confident
+    return (this as any).fromTuple(...decoded);
   }
 }
 
@@ -50,10 +62,10 @@ export class PendingReturn extends TaggedTuple {
 }
 
 export class ScheduleMessage extends TaggedTupleStrings {
-  public static readonly tag = 2
+  public static readonly tag = 2;
 
   public readonly root_id: string;
-  public readonly call_hash: string
+  public readonly call_hash: string;
 
   constructor(root_id: string, call_hash: string) {
     super();
