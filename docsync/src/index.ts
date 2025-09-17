32459246b2a1f709a857407a41ac1d7ce877ad09
Robin <robin.lewis@anterior.com>
2025-09-16 20:20:18 -0400

    docs(docsync): keep python and ts docs in sync (#65)
    * build: JS build refactor
    
    * docs: docsync in python Call
    
    * docs: docsync
    
    * docs: docsync in ts/call
    
    * build: build docsync
    
    * test: run docsync in nix flake check
    
    * docs: docsync get paths as cli args
    
    ---------
    
    Co-authored-by: Shun Ueda <62182668+shunueda@users.noreply.github.com>

---
+++ docsync/src/index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node --enable-source-maps
+
+import { deepStrictEqual } from "node:assert";
+import { join } from "node:path";
+import process from "node:process";
+
+import { getPythonDocsStrings } from "./python.ts";
+import { getTypeScriptDocStrings } from "./typescript.ts";
+
+async function main() {
+  const [pythonDir, tsDir] = process.argv.slice(2);
+  console.log("Comparing", pythonDir, "and", tsDir);
+  const path = {
+    python: join(pythonDir, "src/**/*.py"),
+    typescript: join(tsDir, "src/**/*.ts"),
+  } as const;
+
+  const python = await getPythonDocsStrings(path.python);
+  const ts = await getTypeScriptDocStrings(path.typescript);
+
+  deepStrictEqual(python, ts);
+
+  console.log("Docsync completed successfully");
+}
+
+main();
