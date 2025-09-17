9636810b7224a3226d39b620fe20d4f222a0809b
Shun Ueda <me@shu.nu>
2025-09-16 20:14:56 -0400

    fix(ts): racy add pending return (#67)
    * feat: add tagged-tuple
    
    * tmp
    
    * test: migrate tests
    
    * remove unused
    
    * refactor: tagged tuple as function
    
    * feat: more types
    
    * chore: fmt
    
    * refactor: simplify errors
    
    * chore: cleanup
    
    * refactor: unify encoders
    
    * chore: cleanup
    
    * chore: cleanup
    
    * refactor: rename
    
    * chore: fmt
    
    * fix
    
    * chore: cleanup

--- typescript/src/store.ts
+++ typescript/src/store.ts
@@ -181,29 +181,24 @@ export class Memory {
     };
     let shouldSchedule = false;
     await this.withCas(async () => {
-      let existingEncoded = await this.store.get(memKey);
-      let existing: PendingReturns;
-      if (existingEncoded) {
-        existing = PendingReturns.decode(existingEncoded);
-      } else {
-        existing = new PendingReturns(Math.floor(Date.now() / 1000), []);
-        existingEncoded = existing.encode();
-        /**
-         * TODO this is racy. https://github.com/cohelm/brrr/pull/64
-         */
-        if (!(await this.store.setNewValue(memKey, existingEncoded))) {
-          return false;
-        }
+      shouldSchedule = false;
+      const existingEncoded = await this.store.get(memKey);
+      if (!existingEncoded) {
+        const newReturns = new PendingReturns(Math.floor(Date.now() / 1000), [
+          newReturn,
+        ]);
         shouldSchedule = true;
+        return await this.store.setNewValue(memKey, newReturns.encode());
       }
-      shouldSchedule ||= [...existing.encodedReturns].some((it) =>
+      const pendingReturns = PendingReturns.decode(existingEncoded);
+      shouldSchedule = [...pendingReturns.encodedReturns].some((it) =>
         TaggedTuple.decodeFromString(PendingReturn, it).isRepeatedCall(
           newReturn,
         ),
       );
       const newReturns = new PendingReturns(
-        existing.scheduledAt,
-        existing.encodedReturns
+        pendingReturns.scheduledAt,
+        pendingReturns.encodedReturns
           .union(new Set([TaggedTuple.encodeToString(newReturn)]))
           .values()
           .map((it) => TaggedTuple.decodeFromString(PendingReturn, it)),
