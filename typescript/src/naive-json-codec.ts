1c23e5c07969b6ade254e661e2b75220a916987b
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-09-18 11:23:51 -0400

    use unified codec

--- typescript/src/naive-json-codec.ts
+++ typescript/src/naive-json-codec.ts
@@ -1,7 +1,7 @@
 import { type BinaryToTextEncoding, createHash } from "node:crypto";
-import { TextDecoder, TextEncoder } from "node:util";
 import type { Call } from "./call.ts";
 import type { Codec } from "./codec.ts";
+import { decoder, encoder } from "./internal-codecs.ts";
 
 /**
  * Naive JSON codec that uses built-in `JSON` for serialization and deserialization.
@@ -16,11 +16,8 @@ export class NaiveJsonCodec implements Codec {
   public static readonly binaryToTextEncoding =
     "hex" satisfies BinaryToTextEncoding;
 
-  private static readonly encoder = new TextEncoder();
-  private static readonly decoder = new TextDecoder();
-
   public async decodeReturn(_: string, payload: Uint8Array): Promise<unknown> {
-    const decoded = NaiveJsonCodec.decoder.decode(payload);
+    const decoded = decoder.decode(payload);
     return JSON.parse(decoded);
   }
 
@@ -30,7 +27,7 @@ export class NaiveJsonCodec implements Codec {
   ): Promise<Call> {
     const sortedArgs = args.map(NaiveJsonCodec.sortObjectKeys);
     const data = JSON.stringify(sortedArgs);
-    const payload = NaiveJsonCodec.encoder.encode(data);
+    const payload = encoder.encode(data);
     const callHash = await this.hashCall(taskName, sortedArgs);
     return { taskName, payload, callHash };
   }
@@ -39,11 +36,11 @@ export class NaiveJsonCodec implements Codec {
     call: Call,
     task: (...args: A) => Promise<R>,
   ): Promise<Uint8Array> {
-    const decoded = NaiveJsonCodec.decoder.decode(call.payload);
+    const decoded = decoder.decode(call.payload);
     const args = JSON.parse(decoded) as A;
     const result = await task(...args);
     const resultJson = JSON.stringify(result);
-    return NaiveJsonCodec.encoder.encode(resultJson);
+    return encoder.encode(resultJson);
   }
 
   private async hashCall<A extends unknown>(
