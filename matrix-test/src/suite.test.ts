ff55c7efa0a18f670d739176852519c5f2236916
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2025-09-23 11:01:02 -0400

    chore: cleanup

--- matrix-test/src/suite.test.ts
+++ matrix-test/src/suite.test.ts
@@ -1,7 +1,7 @@
 import { suite, test } from "node:test";
 import { createMatrixSuite, matrixSuite } from "./suite.ts";
 import { deepStrictEqual } from "node:assert";
-import { combinations } from "./matrix.ts";
+import { type Combination, combinations } from "./matrix.ts";
 
 await suite(import.meta.filename, async () => {
   const matrix = {
@@ -9,15 +9,17 @@ await suite(import.meta.filename, async () => {
     arch: ["x64", "arm64"],
   };
 
+  function makeTestName(combination: Combination<typeof matrix>): string {
+    return `os=${combination.os},arch=${combination.arch}`;
+  }
+
   const combinationsExpected = combinations(matrix);
 
-  const suiteNamesExpected = combinationsExpected.map(
-    ({ os, arch }) => `os=${os},arch=${arch}`,
-  );
+  const suiteNamesExpected = combinationsExpected.map(makeTestName);
 
   await test(matrixSuite.name, async () => {
     const combinations: unknown[] = [];
-    await matrixSuite("matrix suite", matrix, async (_, combination) => {
+    await matrixSuite("sample matrix suite", matrix, async (_, combination) => {
       combinations.push(combination);
     });
     deepStrictEqual(combinations, combinationsExpected);
@@ -27,13 +29,13 @@ await suite(import.meta.filename, async () => {
     const combinations: unknown[] = [];
     const names: string[] = [];
 
-    const suite = createMatrixSuite(matrix, (combination) => {
-      return `os=${combination.os},arch=${combination.arch}`;
-    });
-    await suite("matrix suite", async (context, combination) => {
+    const matrixSuite = createMatrixSuite(matrix, makeTestName);
+
+    await matrixSuite("sample matrix suite", async (context, combination) => {
       combinations.push(combination);
       names.push(context.name);
     });
+
     deepStrictEqual(combinations, combinationsExpected);
     deepStrictEqual(names, suiteNamesExpected);
   });
